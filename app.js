82a5b305ede49841978c7f52d4bca48ea47ba770
Greg Samuelson <grssam@gmail.com>
2020-11-13 16:21:41 -0800

    with jQuery loop

--- app.js
+++ app.js
@@ -3,6 +3,36 @@
 date = moment().format("dddd, MMM Do YYYY")
 $('#currentDay').append(date)
 
+for (let i = 8; i < 18; i++) {
+  let row = $('<div>').addClass("row")
+  let timeCol = $(`<div class="col-2 hour">`)
+  let inputField = $(`<div class="col-8 description " id="t${i}">`)
+  let userInput = $(`<input type="text" id="i${i}" class="textarea">`)
+  let saveSection = $(`<div class="col-2 saveBtn i:hover">`)
+  let saveButton = $(`<button class="button" id="b${i}">`)
+  let saveIcon = $(`<img class="icon" src="./assets/save.png" alt="save">`)
+  let deleteButton = $(`<button class="delete " id="d${i}">`)
+  let deleteIcon = $(`<img class="icon" src="./assets/red-x-line.png" alt="delete">`)
+
+  $(".container").append(row)
+  $(row).append(timeCol, inputField, saveSection)
+  $(inputField).append(userInput)
+  $(saveSection).append(saveButton, deleteButton)
+  $(saveButton).append(saveIcon)
+  $(deleteButton).append(deleteIcon)
+
+  if (i > 12) {
+    timeCol.text(`${i - 12}PM`);
+  } else {
+    timeCol.text(`${i}AM`);
+  }
+
+}
+
+
+
+
+
 // ---Get the hour---
 hr = parseInt(moment().format('H'))
 const hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
@@ -23,19 +53,14 @@ setColors()
 
 let wholeDay = JSON.parse(localStorage.getItem('dayPlan'))
 if (wholeDay) {
-
-}
-else {
-  wholeDay = [{
-    time: 0,
-    activity: 'nothing'
-  }]
+  wholeDay.forEach((activity, i) => {
+    $(`#t${wholeDay[i].time}`).text(wholeDay[i].activity)
+  })
+} else {
+  wholeDay = []
 }
 
 
-wholeDay.forEach((activity, i) => {
-  $(`#t${wholeDay[i].time}`).text(wholeDay[i].activity)
-})
 
 
 // ---get user input and write to page---
